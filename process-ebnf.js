53c33f069805b8f0b645ccbb9db4bda53ee7d6a6
Ruben Verborgh <ruben@verborgh.org>
2014-08-14 14:20:10 +0200

    Extract character classes.

--- process-ebnf.js
+++ process-ebnf.js
@@ -3,15 +3,17 @@ var fs = require('fs');
 var lines = fs.readFileSync('SPARQL-EBNF.txt', { encoding: 'utf-8' }).split(/\n/);
 
 // Parse all lines for terminals
-var symbols = {}, terminals = {};
+var symbols = {}, terminals = {}, classes = {};
 lines.forEach(function (line) {
   var parts = line.match(/^(\w+)\s*::=\s*(.*)$/);
   if (parts) {
     var name = parts[1], value = parts[2];
     symbols[name] = value;
+    if (!/[a-z]/.test(name))
+      classes[name] = value;
     var terminalMatcher = /(?:^|\s)(?:'([^']+)'|"([^"]+)")/g, match;
     while ((match = terminalMatcher.exec(value)) && (match = match[1] || match[2]))
-      terminals[match] = match;
+      terminals[match] = toDoubleQuoted(match).replace(/^"(\w+) (\w+)"$/, '"$1"\\s+"$2"');
   }
 });
 
@@ -31,11 +33,16 @@ function toDoubleQuoted(string) {
 function toTerminalId(string) {
   return toSingleQuoted(string).replace(/\s+/g, '');
 }
-
+function getLength(string) {
+  return string.length;
+}
 function pad(string, length) {
   while (string.length < length)
     string += ' ';
   return string;
+}
+function escapeForRegex(string) {
+  return string.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|"]/g, '\\$&');
 }
 
 
@@ -43,18 +50,38 @@ function pad(string, length) {
 
 println('%lex\n');
 
+// Write classes
+var maxLength = Math.max.apply(null, Object.keys(classes).map(getLength)) + 1;
+for (var className in classes) {
+  var value = classes[className];
+  value = value.replace(/#x([0-9A-F]{4})/g, '\\u$1');
+  value = value.replace(/#x([0-9A-F]{3})/g, '\\u0$1');
+  value = value.replace(/#x([0-9A-F]{2})/g, '\\u00$1');
+  value = value.replace(/#x([0-9A-F]{1})/g, '\\u000$1');
+  value = value.replace(/(^|\s|\()([A-Z_]+)/g, '$1{$2}');
+  value = value.replace(/'\\'/g, '"\\\\"');
+  value = value.replace(/'"'/, '"\\""');
+  value = value.replace(/'"""'/, '"\\"\\"\\""');
+  value = value.replace(/'([^'"]+?)'/g, '"$1"').replace(/\s+/g, '');
+  value = value.replace('[\\u10000-\\uEFFFF]', '[\\uD800-\\uDB7F][\\uDC00-\\uDFFF]');
+  value = value.replace(/(\[\^?)([^-]+?)(\])/g, function (match, start, characters, end) {
+    return start + escapeForRegex(characters) + end;
+  });
+  value = value.replace(/\[(.+?)\]-\[(.+?)\]/g, '[$1$2]');
+  println(pad(className, maxLength), value);
+}
+println();
+
 println('%options flex case-insensitive\n');
 
 println('%%\n');
 
 // Write terminals
-terminals = Object.keys(terminals);
-var maxLength = Math.max.apply(null, terminals.map(function (t) { return t.length; })) + 4;
+maxLength = Math.max.apply(null, Object.keys(terminals).map(getLength)) + 4;
 println(pad('\\s+', maxLength), '/* ignore */');
-terminals.forEach(function (terminal) {
-  var pattern = toDoubleQuoted(terminal).replace(/^"(\w+) (\w+)"$/, '"$1"\\s+"$2"');
-  println(pad(pattern, maxLength), "return", toTerminalId(terminal));
-});
+for (var terminal in terminals) {
+  println(pad(terminals[terminal], maxLength), "return", toTerminalId(terminal));
+};
 println(pad("<<EOF>>", maxLength), "return 'EOF'\n");
 
 println('/lex\n');
@@ -63,4 +90,4 @@ println('%%\n');
 
 // Write production rules
 println('query');
-println('  : (' + terminals.map(toTerminalId).join(' | ') + ') EOF;');
+println('  : (' + Object.keys(terminals).map(toTerminalId).join(' | ') + ') EOF;');
