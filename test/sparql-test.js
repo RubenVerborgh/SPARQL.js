41c2dcb28fa72827c5b8b30a342ffa23ba875877
Ruben Verborgh <ruben@verborgh.org>
2014-08-19 21:25:23 +0200

    Order tests by query name.

--- test/sparql-test.js
+++ test/sparql-test.js
@@ -10,13 +10,17 @@ describe('The SPARQL parser', function () {
   // Ensure the same blank node identifiers are used in every test
   beforeEach(function () { SparqlParser.Parser._resetBlanks(); });
 
-  fs.readdirSync(queriesPath).forEach(function (queryFile) {
-    var parsedQueryFile = parsedQueriesPath + queryFile.replace('.sparql', '.json');
+  var queries = fs.readdirSync(queriesPath);
+  queries = queries.map(function (q) { return q.replace(/\.sparql$/, ''); });
+  queries.sort();
+
+  queries.forEach(function (query) {
+    var parsedQueryFile = parsedQueriesPath + query + '.json';
     if (!fs.existsSync(parsedQueryFile)) return;
 
-    it('should correctly parse ' + queryFile, function () {
-      var query = fs.readFileSync(queriesPath + queryFile, 'utf8');
+    it('should correctly parse query "' + query + '"', function () {
       var parsedQuery = JSON.parse(fs.readFileSync(parsedQueryFile, 'utf8'));
+      query = fs.readFileSync(queriesPath + query + '.sparql', 'utf8');
       expect(SparqlParser.parse(query)).to.deep.equal(parsedQuery);
     });
   });
